ed74340d658f9b7b5f89636eb891383f3ea7c495
A Abhiram <abhiram.ampabathina@gmail.com>
2012-10-02 16:13:10 +0530

    first save to the project. Date 10th october 2012. Made the first look.

---
+++ jsinclude.js
@@ -0,0 +1,8 @@
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/script.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<script type="text/javascript" src="js/bootstrap-carousel.js"></script>
+	<script type="text/javascript" src="js/validate.js"></script>
+    <script type="text/javascript" src="js/carousel.js"></script>
